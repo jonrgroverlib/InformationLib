8b38ff3b8765383f3bae303c1152b2dca80ae81d
jonrgrover <34585315+jonrgroverlib@users.noreply.github.com>
2018-03-21 17:37:38 -0400

    Data models

---
+++ DataModels/EndemeProfileTable.cs
@@ -0,0 +1,41 @@
+﻿using InformationLib.Endemes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; // for [Key]
+using System.Linq;
+using System.Text;
+
+namespace InformationLib.DataModels
+{
+    // --------------------------------------------------------------------------------------------
+    /// <!-- EndemeProfileTable -->
+    /// <summary>
+    ///      The EndemeProfileTable class contains objects that parallel the EndemeProfile table
+    /// </summary>
+    public class EndemeProfileTable
+    {
+        // ----------------------------------------------------------------------------------------
+        //  Members
+        // ----------------------------------------------------------------------------------------
+      //[Key]
+        public Guid     EndemeProfileId  { get; set; }  // primary key
+        public string   DataLabel        { get; set; }
+        public string   DataValueString  { get; set; }
+        public string   DataTableName    { get; set; }
+        public int      DataRowId        { get; set; }
+        public string   DataColumnName   { get; set; }
+        public double   ProfileMatch     { get; set; }
+        public string   ProfileTbdColumn { get; set; }
+        public List<EndemeCharacteristic> EndemeCharacteristicList { get; set; }
+
+
+        // ----------------------------------------------------------------------------------------
+        //  Constructor
+        // ----------------------------------------------------------------------------------------
+        public EndemeProfileTable()
+        {
+        }
+
+    }
+
+}
---
+++ DataModels/EndemeTable.cs
@@ -0,0 +1,49 @@
+﻿using InformationLib.Endemes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace InformationLib.DataModels
+{
+    // --------------------------------------------------------------------------------------------
+    /// <!-- EndemeTable -->
+    /// <summary>
+    ///      The EndemeTable class contains objects that parallel the Endeme table
+    /// </summary>
+    public class EndemeTable
+    {
+        // ----------------------------------------------------------------------------------------
+        //  Members
+        // ----------------------------------------------------------------------------------------
+      //[Key]
+        public int       EndemeId        { get; set; }  // primary key
+        public Guid      EndemeSetId     { get; set; }
+        public string    EndemeString    { get; set; }
+        public string    EndemeCode      { get; set; }
+        public string    EndemeLabel     { get; set; }
+        public string    EndemeDescr     { get; set; }
+        public bool      RawValue        { get; set; }
+        public bool      MultipleUse     { get; set; }
+        public byte[]    ValueBinary     { get; set; }
+        public DateTime  ValueDateTime   { get; set; }
+        public double    ValueFloat      { get; set; }
+        public decimal   ValueNumber     { get; set; }
+        public string    ValueText       { get; set; }
+        public int       EndemeProfileId { get; set; }
+        public EndemeSet EndemeSet       { get; set; }
+        public List<EndemeIndex> EndemeIndexList { get; set; }
+
+
+        // ----------------------------------------------------------------------------------------
+        //  Constructor
+        // ----------------------------------------------------------------------------------------
+        public EndemeTable()
+        {
+        }
+
+    }
+
+
+}
